a00c5e227943a99f6ad69934a411e685f795b24f
Beniamino Negrini <beniamino.negrini@gmail.com>
2015-05-20 10:24:30 +0200

    Inserita la classe Model/Indirizzo

---
+++ Model/Indirizzo.class.php
@@ -0,0 +1,44 @@
+<?php
+
+
+class IndirizzoSupermarket {
+     
+private $Via;
+private $Città;
+private $NumeroCivico;
+
+
+
+
+    public function __construct($Via, $Città, $NumeroCivico) {
+        $this->Via=$Via;
+        $this->Città=$Città;
+        $this->NumeroCivico;
+    }
+
+    public function getVia() {
+        return $this->Via;
+        
+    }
+    public function getCittà(){
+    return $this->Città;
+    }
+    public function getNumeroCivico (){
+    return $this->NumeroCivico;
+    }
+
+    
+    public function setVia($Via) {
+    $this->Via= $Via;      
+    }
+    
+    public function setCittà($Città) {
+    $this->Città=$Città;
+    }
+    
+    public function set($NumeroCivico) {
+    $this->NumeroCivico=$NUmeroCivico;
+    }
+
+}
+?>
